5a02d2fa193d83a4b72a66109028641802034f68
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-05-12 14:38:19 -0600

    🎉 First Commit

---
+++ src/application/Application.java
@@ -0,0 +1,56 @@
+package application;
+
+import com.formdev.flatlaf.FlatClientProperties;
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.fonts.roboto.FlatRobotoFont;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import com.formdev.flatlaf.util.UIScale;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import components.Background;
+import forms.DashboardForm;
+import menu.FormManager;
+import raven.popup.GlassPanePopup;
+
+/**
+ *
+ * @author Raven
+ */
+public class Application extends JFrame {
+
+    private final boolean UNDECORATED = !true;
+
+    public Application() {
+        init();
+    }
+
+    private void init() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(UIScale.scale(new Dimension(1366, 768)));
+        setLocationRelativeTo(null);
+        if (UNDECORATED) {
+            setUndecorated(UNDECORATED);
+            setBackground(new Color(0, 0, 0, 0));
+        } else {
+            getRootPane().putClientProperty(FlatClientProperties.FULL_WINDOW_CONTENT, true);
+        }
+        setContentPane(new Background(UNDECORATED));
+        // applyComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        GlassPanePopup.install(this);
+        FormManager.install(this, UNDECORATED);
+        FormManager.showForm(new DashboardForm());
+        // applyComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+    }
+
+    public static void main(String[] args) {
+        FlatRobotoFont.install();
+        FlatLaf.registerCustomDefaultsSource("themes");
+        UIManager.put("defaultFont", new Font(FlatRobotoFont.FAMILY, Font.PLAIN, 13));
+        FlatMacLightLaf.setup();
+        EventQueue.invokeLater(() -> new Application().setVisible(true));
+    }
+}
