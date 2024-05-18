c976af43b284f2fef39d92de58475b7118477273
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-05-18 08:37:48 -0600

    ✨ Add database information to the profile form

--- src/EduManager/Controllers/UserController.java
+++ src/EduManager/Controllers/UserController.java
@@ -1,6 +1,7 @@
 package EduManager.Controllers;
 
 import EduManager.DataBase.Database;
+import EduManager.Entities.Admin;
 import EduManager.Entities.Coordinator;
 import EduManager.Entities.Student;
 import EduManager.Entities.Teacher;
@@ -9,33 +10,59 @@ import lombok.Data;
 
 @Data
 public class UserController {
-  static Student student;
-  static User user;
-  static Teacher teacher;
-  static Coordinator coordinator;
+    static Student student;
+    static User user;
+    static Teacher teacher;
+    static Coordinator coordinator;
+    static Admin admin;
 
-  public static User getUser() {
-    return user;
-  }
+    public static User getUser() {
+        return user;
+    }
+    
+    public static Admin getAdmin() {
+        return admin;
+    }
 
-  public static Student getStudent() {
-    return student;
-  }
+    public static Student getStudent() {
+        return student;
+    }
 
-  public static Teacher getTeacher() {
-    return teacher;
-  }
+    public static Teacher getTeacher() {
+        return teacher;
+    }
 
-  public static Coordinator getCoordinator() {
-    return coordinator;
-  }
-  
-  public static boolean login(String username, String password){
-	  user = Database.login(username,password);
-	  return user != null;
-  }
+    public static Coordinator getCoordinator() {
+        return coordinator;
+    }
 
-  public static void logout() {
-    user = null;
-  }
+    public static boolean login(String username, String password) {
+        user = Database.login(username, password);
+        
+        student = null;
+        teacher = null;
+        coordinator = null;
+	admin = null;
+
+        if (user instanceof Student) {
+            student = (Student) user;
+        } else if (user instanceof Teacher) {
+            teacher = (Teacher) user;
+        } else if (user instanceof Coordinator) {
+            coordinator = (Coordinator) user;
+        } else if(user instanceof Admin){
+	    admin = (Admin) user;
+	}
+
+        return user != null;
+    }
+
+    public static void logout() {
+        user = null;
+        student = null;
+        teacher = null;
+        coordinator = null;
+	admin = null;
+    }
 }
+
--- src/EduManager/Forms/ProfileForm.java
+++ src/EduManager/Forms/ProfileForm.java
@@ -1,26 +1,49 @@
 package EduManager.Forms;
 
 import EduManager.Components.SimpleForm;
+import EduManager.Controllers.UserController;
+import EduManager.Entities.Coordinator;
+import EduManager.Entities.Student;
+import EduManager.Entities.Teacher;
+import EduManager.Entities.User;
 
 /**
  *
  * @author Raven
  */
 public class ProfileForm extends SimpleForm {
-
+	
+    User user = UserController.getUser();
     public ProfileForm() {
         initComponents();
-	myLabelControlNumber.setText("22161096");
-	myLabelCurp.setText("HERCO0402139H");
-	myLabelDate.setText("02/03/2004");
-	myLabelEmail.setText("cristian42134@mail.com");
-	myLabelPhone.setText("95132352312");
-	myLabelGender.setText("Masculino");
-        myLabelAddress.setText("Avenida Revolucion #18");
-        myLabelColony.setText("Alamos");
-        myLabelmunicipality.setText("Montoya");
-        myLabelPostalCode.setText("71233");
-        myLabelState.setText("Oaxaca");	
+        myLabelFirstName.setText(user.getFirstName());
+	myLabelLastName.setText(user.getLastName());
+        myLabelEmail.setText(user.getEmail());
+	myLabelPhone.setText(user.getPhoneNumber());
+        myLabelCurp.setText("NO DISPONIBLE");
+        myLabelDate.setText("NO DISPONIBLE");
+        myLabelGender.setText("NO DISPONIBLE");
+	myLabelControlNumber.setText("NO DISPONIBLE");
+
+        if (user instanceof Student) {
+            Student student = (Student) user;
+            myLabelCurp.setText(student.getCurp());
+            myLabelDate.setText(student.getBirthDate().toString());
+            myLabelGender.setText(String.valueOf(student.getGender()));
+	    myLabelControlNumber.setText(student.getUserId()+"");
+        } else if (user instanceof Teacher) {
+            Teacher teacher = (Teacher) user;
+            myLabelCurp.setText(teacher.getCurp());
+            myLabelDate.setText(teacher.getBirthDate().toString());
+            myLabelGender.setText(String.valueOf(teacher.getGender()));
+	    myLabelControlNumber.setText(teacher.getUserId()+"");
+        } else if (user instanceof Coordinator) {
+            Coordinator coordinator = (Coordinator) user;
+            myLabelCurp.setText(coordinator.getCurp());
+            myLabelDate.setText(coordinator.getBirthDate().toString());
+            myLabelGender.setText(String.valueOf(coordinator.getGender()));
+	    myLabelControlNumber.setText(coordinator.getUserId()+"");
+        } 
     }
 
     @SuppressWarnings("unchecked")
@@ -34,36 +57,28 @@ public class ProfileForm extends SimpleForm {
                 myButtonOutLine2 = new EduManager.Components.MyButtonOutLine();
                 lblUsername2 = new javax.swing.JLabel();
                 lblUsername = new javax.swing.JLabel();
-                myLabelPhone = new EduManager.Components.MyLabel();
-                lblUsername4 = new javax.swing.JLabel();
                 lblUsername5 = new javax.swing.JLabel();
-                lblUsername8 = new javax.swing.JLabel();
-                lblUsername3 = new javax.swing.JLabel();
-                lblUsername9 = new javax.swing.JLabel();
                 lblUsername10 = new javax.swing.JLabel();
-                myLabelName = new EduManager.Components.MyLabel();
+                myLabelFirstName = new EduManager.Components.MyLabel();
                 myLabelDate = new EduManager.Components.MyLabel();
-                myLabelControlNumber = new EduManager.Components.MyLabel();
-                myLabelCurp = new EduManager.Components.MyLabel();
-                myLabelEmail = new EduManager.Components.MyLabel();
                 myLabelGender = new EduManager.Components.MyLabel();
                 jLabel2 = new javax.swing.JLabel();
+                lblUsername11 = new javax.swing.JLabel();
+                myLabelLastName = new EduManager.Components.MyLabel();
                 panelRound3 = new EduManager.Components.PanelRound();
                 lblUsernameContent3 = new javax.swing.JLabel();
                 lblUsername7 = new javax.swing.JLabel();
                 myButtonOutLine3 = new EduManager.Components.MyButtonOutLine();
                 panelRound4 = new EduManager.Components.PanelRound();
                 lblUsernameContent4 = new javax.swing.JLabel();
-                lblUsername12 = new javax.swing.JLabel();
-                lblUsername13 = new javax.swing.JLabel();
-                lblUsername14 = new javax.swing.JLabel();
-                lblUsername15 = new javax.swing.JLabel();
-                lblUsername16 = new javax.swing.JLabel();
-                myLabelAddress = new EduManager.Components.MyLabel();
-                myLabelmunicipality = new EduManager.Components.MyLabel();
-                myLabelState = new EduManager.Components.MyLabel();
-                myLabelColony = new EduManager.Components.MyLabel();
-                myLabelPostalCode = new EduManager.Components.MyLabel();
+                lblUsername3 = new javax.swing.JLabel();
+                myLabelEmail = new EduManager.Components.MyLabel();
+                lblUsername9 = new javax.swing.JLabel();
+                myLabelPhone = new EduManager.Components.MyLabel();
+                myLabelControlNumber = new EduManager.Components.MyLabel();
+                lblUsername8 = new javax.swing.JLabel();
+                lblUsername4 = new javax.swing.JLabel();
+                myLabelCurp = new EduManager.Components.MyLabel();
 
                 setForeground(new java.awt.Color(246, 246, 246));
                 setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -98,43 +113,28 @@ public class ProfileForm extends SimpleForm {
 
                 lblUsername.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
                 lblUsername.setText("Nombre");
-                panelRound1.add(lblUsername, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 220, -1, -1));
-                panelRound1.add(myLabelPhone, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 520, 280, -1));
-
-                lblUsername4.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
-                lblUsername4.setText("Curp");
-                panelRound1.add(lblUsername4, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 580, -1, -1));
+                panelRound1.add(lblUsername, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 190, -1, -1));
 
                 lblUsername5.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
                 lblUsername5.setText("Fecha de Nacimiento");
-                panelRound1.add(lblUsername5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 490, -1, -1));
-
-                lblUsername8.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
-                lblUsername8.setText("Numero de Control");
-                panelRound1.add(lblUsername8, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 400, -1, -1));
-
-                lblUsername3.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
-                lblUsername3.setText("Email");
-                panelRound1.add(lblUsername3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 310, 350, -1));
-
-                lblUsername9.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
-                lblUsername9.setText("Telefono");
-                panelRound1.add(lblUsername9, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 490, -1, -1));
+                panelRound1.add(lblUsername5, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 390, 500, -1));
 
                 lblUsername10.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
                 lblUsername10.setText("Genero");
-                panelRound1.add(lblUsername10, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 580, -1, -1));
-                panelRound1.add(myLabelName, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 250, -1, -1));
-                panelRound1.add(myLabelDate, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 520, 270, -1));
-                panelRound1.add(myLabelControlNumber, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 430, -1, -1));
-                panelRound1.add(myLabelCurp, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 610, 280, -1));
-                panelRound1.add(myLabelEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 340, 570, -1));
-                panelRound1.add(myLabelGender, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 610, 270, -1));
+                panelRound1.add(lblUsername10, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 480, 360, -1));
+                panelRound1.add(myLabelFirstName, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 220, -1, -1));
+                panelRound1.add(myLabelDate, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 420, 570, -1));
+                panelRound1.add(myLabelGender, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 510, 570, -1));
 
                 jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/EduManager/Images/user.png"))); // NOI18N
                 panelRound1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 90, 80, 70));
 
-                jPanel1.add(panelRound1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 150, 690, 680));
+                lblUsername11.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
+                lblUsername11.setText("Apellido");
+                panelRound1.add(lblUsername11, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 290, 130, -1));
+                panelRound1.add(myLabelLastName, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 320, 570, -1));
+
+                jPanel1.add(panelRound1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 150, 690, 590));
 
                 panelRound3.setBackground(new java.awt.Color(255, 255, 255));
                 panelRound3.setMinimumSize(new java.awt.Dimension(80, 80));
@@ -192,32 +192,27 @@ public class ProfileForm extends SimpleForm {
                 lblUsernameContent4.setText("Informacion de Contacto");
                 panelRound4.add(lblUsernameContent4, new org.netbeans.lib.awtextra.AbsoluteConstraints(24, 24, -1, -1));
 
-                lblUsername12.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
-                lblUsername12.setText("Calle y Numero");
-                panelRound4.add(lblUsername12, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 80, -1, -1));
+                lblUsername3.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
+                lblUsername3.setText("Email");
+                panelRound4.add(lblUsername3, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 70, 350, -1));
+                panelRound4.add(myLabelEmail, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 100, 810, -1));
 
-                lblUsername13.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
-                lblUsername13.setText("Colonia");
-                panelRound4.add(lblUsername13, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 170, -1, -1));
+                lblUsername9.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
+                lblUsername9.setText("Telefono");
+                panelRound4.add(lblUsername9, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 170, -1, -1));
+                panelRound4.add(myLabelPhone, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 200, 390, -1));
+                panelRound4.add(myLabelControlNumber, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 200, 390, -1));
 
-                lblUsername14.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
-                lblUsername14.setText("Codigo Postal");
-                panelRound4.add(lblUsername14, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 80, -1, -1));
+                lblUsername8.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
+                lblUsername8.setText("Numero de Control");
+                panelRound4.add(lblUsername8, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 170, 190, -1));
 
-                lblUsername15.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
-                lblUsername15.setText("Municipio");
-                panelRound4.add(lblUsername15, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, -1, -1));
+                lblUsername4.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
+                lblUsername4.setText("Curp");
+                panelRound4.add(lblUsername4, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, 580, -1));
+                panelRound4.add(myLabelCurp, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 290, 810, -1));
 
-                lblUsername16.setFont(new java.awt.Font("CaskaydiaCove NF", 0, 18)); // NOI18N
-                lblUsername16.setText("Estado");
-                panelRound4.add(lblUsername16, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 360, -1, -1));
-                panelRound4.add(myLabelAddress, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 110, 400, -1));
-                panelRound4.add(myLabelmunicipality, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 290, 800, -1));
-                panelRound4.add(myLabelState, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 390, 800, -1));
-                panelRound4.add(myLabelColony, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 200, 800, -1));
-                panelRound4.add(myLabelPostalCode, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 110, 370, -1));
-
-                jPanel1.add(panelRound4, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 370, 860, 460));
+                jPanel1.add(panelRound4, new org.netbeans.lib.awtextra.AbsoluteConstraints(770, 370, 860, 370));
 
                 add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1660, 880));
         }// </editor-fold>//GEN-END:initComponents
@@ -228,11 +223,7 @@ public class ProfileForm extends SimpleForm {
         private javax.swing.JPanel jPanel1;
         private javax.swing.JLabel lblUsername;
         private javax.swing.JLabel lblUsername10;
-        private javax.swing.JLabel lblUsername12;
-        private javax.swing.JLabel lblUsername13;
-        private javax.swing.JLabel lblUsername14;
-        private javax.swing.JLabel lblUsername15;
-        private javax.swing.JLabel lblUsername16;
+        private javax.swing.JLabel lblUsername11;
         private javax.swing.JLabel lblUsername2;
         private javax.swing.JLabel lblUsername3;
         private javax.swing.JLabel lblUsername4;
@@ -245,18 +236,14 @@ public class ProfileForm extends SimpleForm {
         private javax.swing.JLabel lblUsernameContent4;
         private EduManager.Components.MyButtonOutLine myButtonOutLine2;
         private EduManager.Components.MyButtonOutLine myButtonOutLine3;
-        private EduManager.Components.MyLabel myLabelAddress;
-        private EduManager.Components.MyLabel myLabelColony;
         private EduManager.Components.MyLabel myLabelControlNumber;
         private EduManager.Components.MyLabel myLabelCurp;
         private EduManager.Components.MyLabel myLabelDate;
         private EduManager.Components.MyLabel myLabelEmail;
+        private EduManager.Components.MyLabel myLabelFirstName;
         private EduManager.Components.MyLabel myLabelGender;
-        private EduManager.Components.MyLabel myLabelName;
+        private EduManager.Components.MyLabel myLabelLastName;
         private EduManager.Components.MyLabel myLabelPhone;
-        private EduManager.Components.MyLabel myLabelPostalCode;
-        private EduManager.Components.MyLabel myLabelState;
-        private EduManager.Components.MyLabel myLabelmunicipality;
         private EduManager.Components.PanelRound panelRound1;
         private EduManager.Components.PanelRound panelRound3;
         private EduManager.Components.PanelRound panelRound4;
