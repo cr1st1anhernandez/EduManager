2c2a7505f90d5d5802819aa0370b468db22bf699
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-05-17 19:39:29 -0600

    🗃️ Update database

--- SQL/SqlDatabaseScript.sql
+++ SQL/SqlDatabaseScript.sql
@@ -39,7 +39,8 @@ CREATE TABLE Student (
     curp VARCHAR(18) NOT NULL,
     birthDate DATE NOT NULL,
     gender CHAR(1) NOT NULL,
-    area VARCHAR(255) NOT NULL,
+    career VARCHAR(255) NOT NULL,
+    semester INT(1) NOT NULL,
     PRIMARY KEY (studentId)
 );
 
@@ -66,3 +67,97 @@ CREATE TABLE Group_Subject (
     FOREIGN KEY (teacherId) REFERENCES Teacher(teacherId),
     FOREIGN KEY (studentId) REFERENCES Student(studentId)
 );
+
+CREATE TABLE Admin (
+	adminId INT(8) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phoneNumber VARCHAR(20) NOT NULL,
+	PRIMARY KEY (adminId)
+);
+
+-- Inserción de datos en la tabla Teacher
+INSERT INTO Teacher (teacherId, password, firstName, lastName, email, phoneNumber, curp, birthDate, gender, career)
+VALUES
+(10000001, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Ana', 'Lopez', 'ana.lopez@example.com', '555-1234', 'LOPA000101HDFRNN01', '1980-01-01', 'F', 'Mathematics'),
+(10000002, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Carlos', 'Martinez', 'carlos.martinez@example.com', '555-1235', 'MARC800101HDFRNN02', '1982-02-02', 'M', 'Physics'),
+(10000003, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Laura', 'Garcia', 'laura.garcia@example.com', '555-1236', 'GALA800101HDFRNN03', '1984-03-03', 'F', 'Chemistry'),
+(10000004, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Miguel', 'Hernandez', 'miguel.hernandez@example.com', '555-1237', 'HERM800101HDFRNN04', '1986-04-04', 'M', 'Biology'),
+(10000005, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Sofia', 'Perez', 'sofia.perez@example.com', '555-1238', 'PESF800101HDFRNN05', '1988-05-05', 'F', 'History'),
+(10000006, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Juan', 'Gonzalez', 'juan.gonzalez@example.com', '555-1239', 'GOJU800101HDFRNN06', '1990-06-06', 'M', 'Geography'),
+(10000007, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Marta', 'Sanchez', 'marta.sanchez@example.com', '555-1240', 'SAMA800101HDFRNN07', '1992-07-07', 'F', 'Philosophy'),
+(10000008, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Pablo', 'Torres', 'pablo.torres@example.com', '555-1241', 'TOPA800101HDFRNN08', '1994-08-08', 'M', 'Literature'),
+(10000009, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Elena', 'Ramirez', 'elena.ramirez@example.com', '555-1242', 'RAEL800101HDFRNN09', '1996-09-09', 'F', 'Arts'),
+(10000010, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Luis', 'Fernandez', 'luis.fernandez@example.com', '555-1243', 'FELU800101HDFRNN10', '1998-10-10', 'M', 'Music');
+
+-- Inserción de datos en la tabla Coordinator
+INSERT INTO Coordinator (coordinatorId, password, firstName, lastName, email, phoneNumber, curp, birthDate, gender, area)
+VALUES
+(20000001, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Fernando', 'Castro', 'fernando.castro@example.com', '555-2244', 'CAFE800101HDFRNN11', '1980-11-11', 'M', 'Science'),
+(20000002, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Lucia', 'Mendez', 'lucia.mendez@example.com', '555-2245', 'MELU800101HDFRNN12', '1982-12-12', 'F', 'Engineering'),
+(20000003, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Alberto', 'Ruiz', 'alberto.ruiz@example.com', '555-2246', 'RUAL800101HDFRNN13', '1984-01-13', 'M', 'Humanities'),
+(20000004, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Patricia', 'Ortiz', 'patricia.ortiz@example.com', '555-2247', 'ORPA800101HDFRNN14', '1986-02-14', 'F', 'Social Sciences'),
+(20000005, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Roberto', 'Ramos', 'roberto.ramos@example.com', '555-2248', 'RARO800101HDFRNN15', '1988-03-15', 'M', 'Arts'),
+(20000006, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Angela', 'Reyes', 'angela.reyes@example.com', '555-2249', 'REAN800101HDFRNN16', '1990-04-16', 'F', 'Business'),
+(20000007, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Diego', 'Fuentes', 'diego.fuentes@example.com', '555-2250', 'FUDE800101HDFRNN17', '1992-05-17', 'M', 'Law'),
+(20000008, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Natalia', 'Gomez', 'natalia.gomez@example.com', '555-2251', 'GONA800101HDFRNN18', '1994-06-18', 'F', 'Medicine'),
+(20000009, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Ricardo', 'Vega', 'ricardo.vega@example.com', '555-2252', 'VERI800101HDFRNN19', '1996-07-19', 'M', 'Technology'),
+(20000010, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Adriana', 'Morales', 'adriana.morales@example.com', '555-2253', 'MOAD800101HDFRNN20', '1998-08-20', 'F', 'Education');
+
+-- Inserción de datos en la tabla Student
+INSERT INTO Student (studentId, password, firstName, lastName, email, phoneNumber, curp, birthDate, gender, career, semester)
+VALUES
+(30000001, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Jose', 'Navarro', 'jose.navarro@example.com', '555-3344', 'NAJO800101HDFRNN21', '2000-09-21', 'M', 'Engineering', 1),
+(30000002, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Marina', 'Silva', 'marina.silva@example.com', '555-3345', 'SIMA800101HDFRNN22', '2001-10-22', 'F', 'Medicine', 2),
+(30000003, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Fernando', 'Gonzalez', 'fernando.gonzalez@example.com', '555-3346', 'GOFE800101HDFRNN23', '2002-11-23', 'M', 'Law', 3),
+(30000004, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Lucia', 'Reyes', 'lucia.reyes@example.com', '555-3347', 'RELU800101HDFRNN24', '2003-12-24', 'F', 'Business', 4),
+(30000005, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Pedro', 'Ramirez', 'pedro.ramirez@example.com', '555-3348', 'RAPE800101HDFRNN25', '2004-01-25', 'M', 'Arts', 5),
+(30000006, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Ana', 'Cruz', 'ana.cruz@example.com', '555-3349', 'CRAN800101HDFRNN26', '2005-02-26', 'F', 'Education', 6),
+(30000007, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Miguel', 'Torres', 'miguel.torres@example.com', '555-3350', 'TOMI800101HDFRNN27', '2006-03-27', 'M', 'Technology', 7),
+(30000008, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Sofia', 'Gomez', 'sofia.gomez@example.com', '555-3351', 'GOSO800101HDFRNN28', '2007-04-28', 'F', 'Science', 8),
+(30000009, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Juan', 'Martinez', 'juan.martinez@example.com', '555-3352', 'MAJU800101HDFRNN29', '2008-05-29', 'M', 'Humanities', 9),
+(30000010, 'bd9509aba7d1e5ab68bc526195d369f2418f26f044f5bc2c9af3871a486a6bd3', 'Marta', 'Lopez', 'marta.lopez@example.com', '555-3353', 'LOMA800101HDFRNN30', '2009-06-30', 'F', 'Philosophy', 10);
+
+-- Inserción de datos en la tabla Subject
+INSERT INTO Subject (subjectId, name)
+VALUES
+(1, 'Mathematics'),
+(2, 'Physics'),
+(3, 'Chemistry'),
+(4, 'Biology'),
+(5, 'History'),
+(6, 'Geography'),
+(7, 'Philosophy'),
+(8, 'Literature'),
+(9, 'Arts'),
+(10, 'Music');
+
+-- Inserción de datos en la tabla Group
+INSERT INTO `Group` (groupId)
+VALUES
+('4SA'),
+('4SB'),
+('4SC'),
+('2SA'),
+('2SB'),
+('2SC'),
+('3SA'),
+('3SB'),
+('3SC'),
+('2CA');
+
+-- Inserción de datos en la tabla Group_Subject
+INSERT INTO Group_Subject (groupId, subjectId, startTime, endTime, teacherId, studentId)
+VALUES
+('4SA', 1, '08:00:00', '09:00:00', 10000001, 30000001),
+('4SB', 2, '09:00:00', '10:00:00', 10000002, 30000002),
+('4SC', 3, '10:00:00', '11:00:00', 10000003, 30000003),
+('2SA', 4, '11:00:00', '12:00:00', 10000004, 30000004),
+('2SB', 5, '12:00:00', '13:00:00', 10000005, 30000005),
+('2SC', 6, '13:00:00', '14:00:00', 10000006, 30000006),
+('3SA', 7, '14:00:00', '15:00:00', 10000007, 30000007),
+('3SB', 8, '15:00:00', '16:00:00', 10000008, 30000008),
+('3SC', 9, '16:00:00', '17:00:00', 10000009, 30000009),
+('2CA', 10, '17:00:00', '18:00:00', 10000010, 30000010);
--- src/EduManager/DataBase/Database.java
+++ src/EduManager/DataBase/Database.java
@@ -1,115 +1,119 @@
 package EduManager.DataBase;
 
+import EduManager.Entities.Admin;
 import EduManager.Entities.Coordinator;
 import EduManager.Entities.Student;
 import EduManager.Entities.Teacher;
 import EduManager.Entities.User;
-import io.github.cdimascio.dotenv.Dotenv;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.ResultSet;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 import EduManager.Utils.Utilities;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Database {
 
-  DatabaseAccess databaseAccess = new DatabaseAccess();
+	public Database() {
+		DatabaseAccess.connect();
+	}
 
-  public Database() {
-    DatabaseAccess.connect();
-  }
+	public static void main(String[] args) {
+		Database db = new Database();
+	}
 
-  public static void main(String[] args) {
-    createUser("STUDENT", 22169096, "Juan Alberto Rodriguez", "22161090", "juan@example.com", "Ingeniería en Sistemas", "Contraseña9$");
-    createUser("COORDINATOR", 123456, "Pedro Rodriguez Altamirano", "123456", "correo@ejemplo.com", "Ingeniería en Sistemas",
-        "Contraseña9$");
-    createUser("TEACHER", 789012, "Jesus Enrique Jimenez", "789012", "correo@ejemplo.com", "Ingeniería Quimica", "Contraseña9$");
-  }
+	public static User login(String username, String password) {
+		try {
+			DatabaseAccess.connect();
+			String query = "SELECT * FROM STUDENT WHERE studentId = ? AND password = ?";
+			PreparedStatement preparedStatement = DatabaseAccess.getConnection().prepareStatement(query);
+			preparedStatement.setString(1, username);
+			preparedStatement.setString(2, Utilities.hashPassword(password));
+			ResultSet resultSet = preparedStatement.executeQuery();
 
-  public static void createUser(String userType, Integer userId, String name, String curp, String email, String career,
-      String password) {
-    try {
-      DatabaseAccess.connect();
-      String hashedPassword = Utilities.hashPassword(password);
-      String tableName = userType.toUpperCase();
-      String query = "INSERT INTO " + tableName + " (" + tableName.toLowerCase()
-          + "_id, name, curp, email, " +
-          (userType.equals("COORDINATOR") ? "area" : "career") + ", password) VALUES (?, ?, ?, ?, ?, ?)";
-      PreparedStatement preparedStatement = DatabaseAccess.getConnection().prepareStatement(query);
-      preparedStatement.setInt(1, userId);
-      preparedStatement.setString(2, name);
-      preparedStatement.setString(3, curp);
-      preparedStatement.setString(4, email);
-      preparedStatement.setString(5, career);
-      preparedStatement.setString(6, hashedPassword);
-      preparedStatement.executeUpdate();
-      System.out.println(userType + " created successfully!");
-      DatabaseAccess.closeConnection();
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-  }
+			if (resultSet.next()) {
+				Student student = new Student();
+				student.setUserId(resultSet.getInt("studentId"));
+				student.setFirstName(resultSet.getString("firstName"));
+				student.setLastName(resultSet.getString("lastName"));
+				student.setCurp(resultSet.getString("curp"));
+				student.setPhoneNumber(resultSet.getString("phoneNumber"));
+				student.setEmail(resultSet.getString("email"));
+				student.setCareer(resultSet.getString("career"));
+				student.setSemester(resultSet.getInt("semester"));
+				student.setBirthDate(resultSet.getDate("birthDate"));
+				String genderStr = resultSet.getString("gender");
+				if (genderStr != null && !genderStr.isEmpty()) {
+					student.setGender(genderStr.charAt(0));
+				}
+				return student;
+			}
 
-  public static User login(String username, String password) {
-    try {
-      DatabaseAccess.connect();
-      String query = "SELECT * FROM STUDENT WHERE student_id = ? AND password = ?";
-      PreparedStatement preparedStatement = DatabaseAccess.getConnection().prepareStatement(query);
-      preparedStatement.setString(1, username);
-      preparedStatement.setString(2, Utilities.hashPassword(password));
-      ResultSet resultSet = preparedStatement.executeQuery();
-      if (resultSet.next()) {
-        Student student = new Student();
-        student.setStudentId(resultSet.getInt("student_id"));
-        student.setName(resultSet.getString("name"));
-        student.setCurp(resultSet.getString("curp"));
-        student.setEmail(resultSet.getString("email"));
-        student.setCareer(resultSet.getString("career"));
-        DatabaseAccess.closeConnection();
-        return student;
-      }
+			query = "SELECT * FROM TEACHER WHERE teacherId = ? AND password = ?";
+			preparedStatement = DatabaseAccess.getConnection().prepareStatement(query);
+			preparedStatement.setString(1, username);
+			preparedStatement.setString(2, Utilities.hashPassword(password));
+			resultSet = preparedStatement.executeQuery();
 
-      query = "SELECT * FROM TEACHER WHERE teacher_id = ? AND password = ?";
-      preparedStatement = DatabaseAccess.getConnection().prepareStatement(query);
-      preparedStatement.setString(1, username);
-      preparedStatement.setString(2, Utilities.hashPassword(password));
-      resultSet = preparedStatement.executeQuery();
-      if (resultSet.next()) {
-        Teacher teacher = new Teacher();
-        teacher.setTeacherId(resultSet.getInt("teacher_id"));
-        teacher.setName(resultSet.getString("name"));
-        teacher.setCurp(resultSet.getString("curp"));
-        teacher.setEmail(resultSet.getString("email"));
-        teacher.setCareer(resultSet.getString("career"));
-        DatabaseAccess.closeConnection();
-        return teacher;
-      }
+			if (resultSet.next()) {
+				Teacher teacher = new Teacher();
+				teacher.setUserId(resultSet.getInt("teacherId"));
+				teacher.setLastName(resultSet.getString("lastName"));
+				teacher.setFirstName(resultSet.getString("firstName"));
+				teacher.setCurp(resultSet.getString("curp"));
+				teacher.setEmail(resultSet.getString("email"));
+				teacher.setPhoneNumber(resultSet.getString("phoneNumber"));
+				teacher.setBirthDate(resultSet.getDate("birthDate"));
+				String genderStr = resultSet.getString("gender");
+				if (genderStr != null && !genderStr.isEmpty()) {
+					teacher.setGender(genderStr.charAt(0));
+				}
+				teacher.setCareer(resultSet.getString("career"));
+				return teacher;
+			}
 
-      query = "SELECT * FROM COORDINATOR WHERE coordinator_id = ? AND password = ?";
-      preparedStatement = DatabaseAccess.getConnection().prepareStatement(query);
-      preparedStatement.setString(1, username);
-      preparedStatement.setString(2, Utilities.hashPassword(password));
-      resultSet = preparedStatement.executeQuery();
-      if (resultSet.next()) {
-        Coordinator coordinator = new Coordinator();
-        coordinator.setCoordinatorId(resultSet.getInt("coordinator_id"));
-        coordinator.setName(resultSet.getString("name"));
-        coordinator.setCurp(resultSet.getString("curp"));
-        coordinator.setEmail(resultSet.getString("email"));
-        coordinator.setArea(resultSet.getString("area"));
-        DatabaseAccess.closeConnection();
-        return coordinator;
-      }
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
-    DatabaseAccess.closeConnection();
-    return null;
-  }
+			query = "SELECT * FROM COORDINATOR WHERE coordinatorId = ? AND password = ?";
+			preparedStatement = DatabaseAccess.getConnection().prepareStatement(query);
+			preparedStatement.setString(1, username);
+			preparedStatement.setString(2, Utilities.hashPassword(password));
+			resultSet = preparedStatement.executeQuery();
 
+			if (resultSet.next()) {
+				Coordinator coordinator = new Coordinator();
+				coordinator.setUserId(resultSet.getInt("coordinatorId"));
+				coordinator.setFirstName(resultSet.getString("firstName"));
+				coordinator.setLastName(resultSet.getString("lastName"));
+				coordinator.setCurp(resultSet.getString("curp"));
+				coordinator.setEmail(resultSet.getString("email"));
+				coordinator.setCurp(resultSet.getString("curp"));
+				coordinator.setEmail(resultSet.getString("email"));
+				coordinator.setPhoneNumber(resultSet.getString("phoneNumber"));
+				coordinator.setBirthDate(resultSet.getDate("birthDate"));
+				String genderStr = resultSet.getString("gender");
+				if (genderStr != null && !genderStr.isEmpty()) {
+					coordinator.setGender(genderStr.charAt(0));
+				}
+				coordinator.setArea(resultSet.getString("area"));
+				return coordinator;
+			}
+
+			query = "SELECT * FROM ADMIN WHERE adminId = ? AND password = ?";
+			preparedStatement = DatabaseAccess.getConnection().prepareStatement(query);
+			preparedStatement.setString(1, username);
+			preparedStatement.setString(2, Utilities.hashPassword(password));
+			resultSet = preparedStatement.executeQuery();
+			if (resultSet.next()) {
+				Admin admin = new Admin();
+				admin.setUserId(resultSet.getInt("adminId"));
+				admin.setPhoneNumber(resultSet.getString("phoneNumber"));
+				admin.setEmail(resultSet.getString("email"));
+				admin.setFirstName(resultSet.getString("firstName"));
+				admin.setLastName(resultSet.getString("lastName"));
+				return admin;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DatabaseAccess.closeConnection();
+		}
+		return null;
+	}
 }
